3f260aecb4c70b70f3beeeb8dd5a4a078dd11a14
vikram7102 <bhosalevikram7102@gmail.com>
2022-04-14 15:07:06 +0530

    first commit

---
+++ src/HOC/Home.hoc.jsx
@@ -0,0 +1,24 @@
+import React from "react";
+import {  Route } from "react-router-dom";
+
+// Layout
+import HomeLayout from "../layouts/Homepage.layout";
+
+function HomeLayoutHoc({ component: Component,  ...rest }) {
+  return (
+    <>
+      
+      <Route
+        {...rest}
+        component={(props) => (
+          <HomeLayout>
+            <Component {...props} />
+          </HomeLayout>
+        )}
+        />
+      
+    </>
+  );
+}
+
+export default HomeLayoutHoc;
